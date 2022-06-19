02a26a1eab38fad1fa01e0b72defb39360856579
Jonathan Linowes <jonathan@parkerhill.com>
2022-06-19 12:45:02 -0400

    start button

--- screens/Start.js
+++ screens/Start.js
@@ -64,6 +64,16 @@ export default class Start extends React.Component {
     }).start();
   }
 
+  handlePressStart = async () => {
+    const { onStartGame } = this.props;
+
+    await configureTransition(() => {
+      this.setState({ transitionState: State.WillTransitionOut });
+    });
+
+    onStartGame();
+  };
+
   render() {
     const { size, onChangeSize } = this.props;
     const { transitionState } = this.state;
@@ -72,25 +82,30 @@ export default class Start extends React.Component {
     const buttonStyle = { opacity: this.buttonOpacity };
 
     return (
-      <View style={styles.container}>
-        <View style={styles.logo}>
-          <Logo />
+      transitionState !== State.WillTransitionOut && (
+        <View style={styles.container}>
+          <View style={styles.logo}>
+            <Logo />
+          </View>
+          {transitionState !== State.Launching && (
+            <Animated.View style={toggleStyle}>
+              <Toggle
+                options={BOARD_SIZES}
+                value={size}
+                onChange={onChangeSize}
+              />
+            </Animated.View>
+          )}
+          {transitionState !== State.Launching && (
+            <Animated.View style={buttonStyle}>
+              <Button
+                title={'Start Game'}
+                onPress={this.handlePressStart}
+              />
+            </Animated.View>
+          )}
         </View>
-        {transitionState !== State.Launching && (
-          <Animated.View style={toggleStyle}>
-            <Toggle
-              options={BOARD_SIZES}
-              value={size}
-              onChange={onChangeSize}
-            />
-          </Animated.View>
-        )}
-        {transitionState !== State.Launching && (
-          <Animated.View style={buttonStyle}>
-            <Button title={'Start Game'} onPress={() => {}} />
-          </Animated.View>
-        )}
-      </View>
+      )
     );
   }
 }
